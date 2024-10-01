719c49d7d07167e23b8e76199a3b27461dd74b5b
Min Myat Aung <minmyataung20@gmail.com>
2024-10-01 23:07:19 +0700

    Google Auth Pushed

--- src/GoogleAuth.js
+++ src/GoogleAuth.js
@@ -2,9 +2,6 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
 require('dotenv').config();
 
-
-// console.log(process.env.GOOGLE_CLIENT_ID);
-
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
