d3f8a9537c734342907d1d0b4494cdd1c3cef031
nutkung1 <nutrock123123@gmail.com>
2024-10-30 13:13:16 +0700

    Update cart menu order history

--- Login/schema.js
+++
@@ -1,8 +0,0 @@
-// For customer login
-{
-    "_id": ObjectId(),  // Automatically generated unique identifier;
-    "username": String,  // The user's display name or chosen username
-    "google_id": String,  // Google account ID (provided by Google OAuth)
-    "email": String,  // User's email address associated with Google account
-    "created_at": Date  // Timestamp of account creation
-  }
