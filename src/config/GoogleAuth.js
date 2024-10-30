0f5fa92169b7032e51020221744be4b423d5308c
Min Myat Aung <minmyataung20@gmail.com>
2024-10-30 14:52:16 +0700

    Customer credentials database configured

--- src/config/GoogleAuth.js
+++ src/config/GoogleAuth.js
@@ -2,58 +2,84 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
 require('dotenv').config();
 const User = require('../models/userSchema'); 
+const Customer = require('../models/Customer_credential');
 const { generateAndSaveQRCode } = require('./qrCodeUtil');
 
-passport.use(new GoogleStrategy({
+// Google Strategy for StallOwner
+passport.use('google-stallowner', new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback",
+    callbackURL: "http://localhost:3000/auth/stallowner/google/callback",
     passReqToCallback: true,
   },
-  async function(request, accessToken, refreshToken, profile, done) {
+  async (request, accessToken, refreshToken, profile, done) => {
     try {
       let existingUser = await User.findOne({ googleID: profile.id });
       if (existingUser) {
-        console.log('User already exists:', existingUser);
+        console.log("User already exists", existingUser);
         return done(null, existingUser);
       } else {
         const newUser = new User({
           username: profile.displayName,
           googleID: profile.id
         });
-        
-
         const qrCodeUrl = await generateAndSaveQRCode(newUser._id);
-
-        const responseData = {
-          userID: newUser._id,
-          username: newUser.username, 
-          qrCodeUrl: qrCodeUrl,
-        };
+        newUser.qrcode_url = qrCodeUrl;
         await newUser.save();
-      console.log('New User Created:', newUser);
+        console.log("New user created", newUser);
         return done(null, newUser);
-        
       }
-      
     } catch (err) {
-      console.error('Error saving user to the database:', err);
       return done(err, false);
     }
   }
 ));
 
-passport.serializeUser(function(user, done) {
-    done(null, user.id);
+// Google Strategy for Customer
+passport.use('google-customer', new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "http://localhost:3000/auth/customer/google/callback",
+  passReqToCallback: true,
+},
+async (request, accessToken, refreshToken, profile, done) => {
+  try {
+    let existingUser = await Customer.findOne({ googleID: profile.id });
+    if (existingUser) {
+      console.log("Customer already exists", existingUser);
+      return done(null, existingUser);
+    } else {
+      const newCustomer = new Customer({
+        username: profile.displayName,
+        googleID: profile.id
+      });
+      await newCustomer.save();
+      console.log("New customer created", newCustomer);
+      return done(null, newCustomer);
+    }
+  } catch (err) {
+    return done(err, false);
+  }
+}
+));
+
+
+passport.serializeUser((user, done) => {
+  const userType = user instanceof User ? 'StallOwner' : 'Customer';
+  done(null, { id: user.id, userType });
 });
 
-passport.deserializeUser(async function(id, done) {
-    try {
-      const user = await User.findById(id);
-      done(null, user);
-    } catch (err) {
-      done(err, false);
-    }
+
+passport.deserializeUser(async (user, done) => {
+  const { id, userType } = user;
+  try {
+    const foundUser = userType === 'StallOwner'
+      ? await User.findById(id)
+      : await Customer.findById(id);
+    done(null, foundUser);
+  } catch (error) {
+    done(error, null);
+  }
 });
 
 module.exports = passport;
--- src/config/qrCodeUtil.js
+++ src/config/qrCodeUtil.js
@@ -9,39 +9,40 @@ const { PutObjectCommand } = require('@aws-sdk/client-s3');
  * @param {ObjectId} userId 
  * @returns {Promise<string>} 
  */
-
 async function generateAndSaveQRCode(userID) {
     try {
-        const menuUrl = `http://192.168.1.34:3000/protected/${userID}/menu`;
+        const menuUrl = `http://localhost:3000/stallowner/dashboard/${userID}/menu`;
         const qrCodeDataUrl = await QRCode.toDataURL(menuUrl);
         const base64Data = qrCodeDataUrl.replace(/^data:image\/png;base64,/, "");
         const buffer = Buffer.from(base64Data, 'base64');
 
-        const s3Params =  {
-            Bucket : process.env.AWS_BUCKET_NAME,
-            Key : `qrcodes/${userID}-${uuidv4()}.png`,
-            Body : buffer,
-            ContentEncoding : 'base64',
+        const s3Params = {
+            Bucket: process.env.AWS_BUCKET_NAME,
+            Key: `qrcodes/${userID}-${uuidv4()}.png`,
+            Body: buffer,
+            ContentEncoding: 'base64',
             ContentType: 'image/png',
         };
 
         const command = new PutObjectCommand(s3Params);
         await s3Client.send(command);
         const qrCodeUrl = `https://${process.env.AWS_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${s3Params.Key}`;
-    
-    
-        const menu = new Menu({
-            seller : userID,
-            items : [],
-            qrcode_url : qrCodeUrl,
-        });
-    await menu.save();
-    console.log('QR code generated and saved to s3:', qrCodeUrl);
-    return qrCodeUrl;
 
-    }catch (err) {
-    console.error('Error saving user to the database:', err);
-    throw err;
+        const menu = new Menu({
+            seller: userID,
+            items: [], 
+            qrcode_url: qrCodeUrl,
+        });
+        
+        await menu.save();  
+        console.log('QR code generated and saved to S3 and Menu schema:', qrCodeUrl);
+        
+        return qrCodeUrl;
+
+    } catch (err) {
+        console.error('Error generating or saving QR code:', err);
+        throw err;
     }
 }
+
 module.exports = { generateAndSaveQRCode };
