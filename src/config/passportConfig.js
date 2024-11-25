8301d02ec3c26c78b5d66cb0ab9da72fa1322192
KUNG <thanaphat.ngoe@gmail.com>
2024-11-25 16:50:39 +0700

    Copied all latest combind Back-End files from BackEnd to BackEnd_Combind folder in main

--- src/config/passportConfig.js
+++
@@ -1,7 +0,0 @@
-const passport = require('passport');
-const session = require('express-session');
-
-module.exports = (app) => {
-	app.use(passport.initialize());
-	app.use(passport.session());
-};
